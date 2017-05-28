112d6a8c5fdf0bdb54f206ae2190c109ce98c1b3
André Augusto Tissot <andrehtissot@gmail.com>
2017-05-28 17:34:00 -0300

    Little improvements

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-cd ./firebase-extended-notification-app-example
+PROJECT_DIR=$(dirname "$0")
+cd "$PROJECT_DIR/firebase-extended-notification-app-example"
 cordova platform add android
 cordova plugin add https://github.com/andrehtissot/cordova-plugin-firebase-extended-notification

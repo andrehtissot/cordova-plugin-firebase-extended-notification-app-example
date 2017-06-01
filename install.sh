40f19e4ee52e4051ae762bdc4b36e4b508ea0432
André Augusto Tissot <andrehtissot@gmail.com>
2017-06-01 12:50:57 -0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 PROJECT_DIR=$(dirname "$0")
 cd "$PROJECT_DIR/app-example"
+cordova platform remove android
 cordova platform add android
+git checkout platforms/android/res/raw/lost_european_the_beginning_of_the_end_mp3

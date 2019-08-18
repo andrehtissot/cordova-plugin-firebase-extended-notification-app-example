38165f4707038fd7760df6ce4a92daa43117da6d
André Augusto Tissot <andre.tissot@mytheresa.com>
2019-08-18 18:38:33 +0200

    Improved server-side/notify.php script

--- server-side/notify.php
+++ server-side/notify.php
@@ -1,13 +1,37 @@
 <?php
 
-$fields = array(
-  'to' => $_REQUEST['to'],
-  'data' => array(
-    'notificationOptions' => array(
-      'text' => 'Test message',
+$isWeb = !empty($_REQUEST['target']);
+
+$serverKey = getenv('FB_SERVER_KEY');
+if(empty($serverKey)) {
+    exit("FB_SERVER_KEY env variable should be given.\n");
+}
+
+if($isWeb) {
+  $target = $_REQUEST['target'];
+} else {
+  $target = getenv('TARGET');
+  if(empty($target)) {
+      $errorMessage = <<<EOM
+For the specific device or group, please inform the device token or topic, respectively, as:
+\t- 'TARGET' env variable; or
+\t- 'target' request argument.\n
+EOM;
+      exit($errorMessage);
+  }
+}
+
+
+$now = time();
+
+$fields = [
+  'to' => $target,
+  'data' => [
+    'notificationOptions' => [
+      'text' => "Test message at $now",
       // 'summary' => "4 messages",
-      // 'textLines' => array("Message 1", "Message 2", "Message 3", "Message 4"),
-      'title' => 'Title test',
+      // 'textLines' => ["Message 1", "Message 2", "Message 3", "Message 4"),
+      'title' => "Title test at $now",
       'smallIcon' => 'mipmap/icon',
       // 'largeIcon' => 'https://avatars2.githubusercontent.com/u/1174345?v=3&s=96',
       // 'bigPicture' => "https://cloud.githubusercontent.com/assets/7321362/24875178/1e58d2ec-1ddc-11e7-96ed-ed8bf011146c.png",
@@ -22,14 +46,14 @@ $fields = array(
       'autoCancel' => true,
       // 'openApp' => true,
       'priority' => 'high'
-    )
-  )
-);
+    ]
+  ]
+];
 
-$headers = array(
-  'Authorization:key='.AUTHORIZATION_KEY,
+$headers = [
+  "Authorization:key=$serverKey",
   'Content-Type:application/json'
-);
+];
 
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
@@ -42,9 +66,10 @@ $result = curl_exec($ch);
 curl_close($ch);
 $result = json_decode($result, true);
 
-echo '<pre>fields:';
+if($isWeb) {
+  echo '<pre>';
+}
+echo 'fields:';
 print_r($fields);
-echo "\n\nheaders:";
-print_r($headers);
 echo "\n\nresult:";
 print_r($result);

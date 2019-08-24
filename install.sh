2af1ba3dc23884c1a61f2da15f33396f845634d3
André Augusto Tissot <andre.tissot@mytheresa.com>
2019-08-24 20:24:16 +0200

    Commentend mp3 download for now, having it downloading every install execution is less than ideal

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ cd "$PROJECT_DIR/app-example"
 cordova platform remove android
 cordova platform add android@latest
 
-RAW_DIR="platforms/android/app/src/main/res/raw"
-if [ ! -f "$RAW_DIR/lost_european_the_beginning_of_the_end_mp3" ]; then
-    mkdir $RAW_DIR
-    wget https://github.com/andrehtissot/cordova-plugin-firebase-extended-notification-app-example/raw/ad3e26fa5ff9ce93b35f7e861c912e5560637ece/app-example/platforms/android/res/raw/lost_european_the_beginning_of_the_end_mp3 -O "$RAW_DIR/lost_european_the_beginning_of_the_end_mp3"
-fi
+# RAW_DIR="platforms/android/app/src/main/res/raw"
+# if [ ! -f "$RAW_DIR/lost_european_the_beginning_of_the_end_mp3" ]; then
+#     mkdir $RAW_DIR
+#     wget https://github.com/andrehtissot/cordova-plugin-firebase-extended-notification-app-example/raw/ad3e26fa5ff9ce93b35f7e861c912e5560637ece/app-example/platforms/android/res/raw/lost_european_the_beginning_of_the_end_mp3 -O "$RAW_DIR/lost_european_the_beginning_of_the_end_mp3"
+# fi
